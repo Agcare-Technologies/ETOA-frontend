7ad34d91588f5eced97d1f501a19447ad24ea416
Nishant Chaudhary <nishantchaudhary5338@gmail.com>
2023-10-19 13:57:02 +0530

    initial commit, project setup with React + tailwind

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import { useState } from "react";
+import logo from "./logo.svg";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return <div className='text-red-500'>APP HOME</div>;
+}
+
+export default App;
